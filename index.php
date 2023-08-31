d316e870429265ddef82ab6fee4144d7f927171c
Nils Odin <nodin@kth.se>
2023-08-31 06:54:04 +0200

    Debugging

--- index.php
+++ index.php
@@ -14,12 +14,14 @@ if($_SERVER['SERVER_NAME'] === 'www.nilsodin.se') {
 
 ?>
 
+
+
 <html>
 	<head>
 		<title>Nils Odin | Portfolio</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="/assets/css/main.css" />
+		<link rel="stylesheet" href="<?=$prefix?>/assets/css/main.css" />
 
 
 	</head>
