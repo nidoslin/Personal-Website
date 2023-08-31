05c882de87148621f8e5f1746a867b22c7249608
Nils Odin <nodin@kth.se>
2023-08-31 06:49:59 +0200

    Debugging

--- index.php
+++ index.php
@@ -4,6 +4,16 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
+
+<?php
+$prefix = "";
+if($_SERVER['SERVER_NAME'] === 'www.nilsodin.se') {
+	echo "<script>alert('SERVER');</script>"
+	$prefix = "nilsodin.se//";
+} 
+
+?>
+
 <html>
 	<head>
 		<title>Nils Odin | Portfolio</title>
