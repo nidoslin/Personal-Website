f0df6e596485872b940ada8b3dfd80c564c0baeb
Nils Odin <nodin@kth.se>
2023-08-31 07:30:40 +0200

    Debug

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <?php
 // Fix for changing relative paths between server and local env
 $prefix = "";
+echo($_SERVER['SERVER_NAME']);
 if($_SERVER['SERVER_NAME'] === 'nilsodin.se') { 
 	$prefix = "nilsodin.se/"; 
 }
